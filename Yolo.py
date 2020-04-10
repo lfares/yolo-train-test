da1a25ce0450879d0120a1b1d2226b5304b4e795
lfares <livia.fares97@gmail.com>
2020-04-10 12:33:43 -0300

    Fixed error with rectangle function and added return

--- Yolo.py
+++ Yolo.py
@@ -1,12 +1,12 @@
 import os
-import cv2 as cv
+import cv2
 from pydarknet import Detector, Image
 
 class Yolo():
 
     def __init__(self, weights_file='yolov3-2obj_final', darknet_files_path="darknet_files"):
-        self.pinga_color = (55, 145, 6) # green
-        self.emily_color = (6, 81, 156) # blue
+        self.pinga_color = (6, 145, 55) # green
+        self.emily_color = (156, 81, 6) # blue
 
         # Initialize Yolo
         self.net = Detector(bytes(os.path.join(darknet_files_path,"yolov3-2obj.cfg"), encoding="utf-8"), 
@@ -16,14 +16,15 @@ class Yolo():
     def detect (self, frame):
         image_output = Image(frame)
         results = self.net.detect(image_output, thresh=0.85,nms=0.15)
+        del image_output
 
         for cat, score, bounds in results:
             x, y, w, h = bounds
             if cat.decode("utf-8") == "pinga":
-                cv.rectangle(image_output, (int(x-w/2), int(y-h/2)), (int(x+w/2), int(y+h/2)), 
-                    self.pinga_color, thickness=3)
+                cv2.rectangle(frame, (int(x-w/2),int(y-h/2)),(int(x+w/2),int(y+h/2)), self.pinga_color, 3)
             elif cat.decode("utf-8") == "emily":
-                cv.rectangle(image_output, (int(x-w/2), int(y-h/2)), (int(x+w/2), int(y+h/2)), 
-                    self.emily_color, thickness=3)
-            cv.putText(image_output, str(cat.decode("utf-8")), (int(x),int(y)),
-                        cv.FONT_HERSHEY_COMPLEX, 2, (255,255,255))
+                cv2.rectangle(frame, (int(x-w/2),int(y-h/2)),(int(x+w/2),int(y+h/2)), self.emily_color, 3)
+            cv2.putText(frame, str(cat.decode("utf-8")), (int(x),int(y)),
+                        cv2.FONT_HERSHEY_COMPLEX, 3, (255,255,255))
+
+        return frame
